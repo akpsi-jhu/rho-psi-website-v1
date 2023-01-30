8c206025f5cff70fe73509b3ec060acb7e2c2236
Anthony Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2023-01-30 01:56:45 -0500

    icon updated and stuff

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
         <div className="App">
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path='/' element={<Home/>}></Route>
                     <Route path='/contact' element={<Contact/>}></Route>
