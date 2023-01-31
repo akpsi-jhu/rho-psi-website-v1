a5629ceb1f3b0998c9a038f08ca69a06acaa6ddd
Liwen Tran <ltran29@jhu.edu>
2023-01-31 18:23:12 -0500

    Updated ExecSection to more on one row

--- web-app/src/Components/ExecSection.js
+++ web-app/src/Components/ExecSection.js
@@ -23,7 +23,7 @@ const ExecSection = (props) => {
         >
             <Typography variant="h4" marginBottom={3} py={4}>Executive Board</Typography>
             <Box>
-                <Grid justifyContent="center" container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 9, md: 20, lg: 20}}>
+                <Grid justifyContent="center" container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 9, md: 24, lg: 24}}>
                     {pcBrothers.map((brother, index) => (
                         <Grid item xs={2} sm={3} md={4} lg={4} key={index}>
                             <BrotherCard
