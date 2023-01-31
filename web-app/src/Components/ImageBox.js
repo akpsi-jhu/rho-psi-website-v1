59b0d398a0dbfc795c4b6e32742bf0a8449465c5
Anthony Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2023-01-31 12:31:09 -0500

    adjusted image sizing

--- web-app/src/Components/ImageBox.js
+++ web-app/src/Components/ImageBox.js
@@ -7,7 +7,7 @@ const ImageBox = (props) => {
             pl={{lg:5}}
             className="container"
             maxHeight={{xs: "75vh", sm: "75vh", md: "75vh", lg: "65vh", xl: "60vh"}}
-            width={{xs: "115%", sm: "300%", md: "275%", lg: "160%", xl: "150%", xxl: "68%"}}
+            width={{xs: "115%", sm: "300%", md: "300%", lg: "160%", xl: "150%", xxl: "125%"}}
             maxWidth={{sm: "100vw", md: "60vw"}}>
             <img className="container" src={props.src} alt=""/>
         </Box>
