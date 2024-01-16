bd8663e06b154acb431e5b81a42ec485e63f3e4f
Anthony Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2024-01-15 19:49:34 -0500

    added resume review form, feedback form

--- web-app/src/Pages/ActivePortal.js
+++ web-app/src/Pages/ActivePortal.js
@@ -18,17 +18,25 @@ const ActivePortal = () => {
   };
 
   return (
-    <Stack alignItems='center'  sx={{ minHeight: '100vh', justifyContent: 'space-between' }}>
+    <Stack alignItems='center' sx={{ minHeight: '100vh', justifyContent: 'space-between' }}>
       <Navbar toggle={toggle} blue={true}></Navbar>
-      <SideBar isOpen={isOpen} toggle={toggle}/>  
-      <Box maxWidth={1920}  sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 8, marginTop: 20, marginBottom: 20 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
+      <SideBar isOpen={isOpen} toggle={toggle}/>
+      <Box maxWidth={1920} sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', mt: 8, marginTop: 20, marginBottom: 20 }}>
+        <Typography variant="h4" component="h1" gutterBottom> 
           Alumni Directory
         </Typography>
+        <Stack direction={'row'} gap={2}>
+        <Button variant="outlined" href="https://forms.gle/wAHkJjDqpEvLKBNz6" target='blank' sx={{ mt: 2, mb: 2, color: "#0C406F", borderColor: "#0C406F" }}>
+              Resume Review Form
+            </Button>
+            <Button variant="outlined" href="https://forms.gle/kHQbinXL6DKGDUjg6" target='blank' sx={{ mt: 2, mb: 2, color: "#0C406F", borderColor: "#0C406F" }}>
+              Feedback Form
+            </Button>
+            <Button variant="outlined"  onClick={handleLogout} sx={{ mt: 2, mb: 2, color: "#0C406F", borderColor: "#0C406F" }}>
+              Logout
+            </Button>
+        </Stack>
         <AlumniDatabase></AlumniDatabase>
-        <Button variant="contained" onClick={handleLogout} sx={{ backgroundColor: "#0C406F", mt: 2 }}>
-          Logout
-        </Button>
       </Box>
       <Footer></Footer>
     </Stack>
