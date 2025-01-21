a83705220e96069aa8f932d52aac12288dbcbbc9
Anthony Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2025-01-21 08:42:31 -0500

    Updates

--- web-app/src/api/index.js
+++
@@ -1,7 +0,0 @@
-import {BrotherApi} from "./Firestore/BrotherApi";
-// import FakeApi from "./Fake/FakeApi";
-//const Api = new FakeApi();
-
-const Api = new BrotherApi();
-
-export {Api as BrotherApi};
